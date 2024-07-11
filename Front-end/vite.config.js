78f30eff06e6a54294a0ca7dce60880f18722515
HaveaHugg <tech@hugg.co.in>
2024-07-11 23:27:26 +0530

    pushed production changes

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "https://web-technology-jllh.onrender.com/",
       // "/api": "https://InfoSystem-c4541e1462c2.herokuapp.com/",
     },
   },
